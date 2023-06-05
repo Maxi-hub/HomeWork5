f19b147a9b8daf6a1b30f750e3c2af734b261cf9
Emma <em.lebedeva85@gmail.com>
2023-06-05 16:46:44 +0300

    Update Task38

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -2,24 +2,31 @@
 // [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
 
 
-double[] array = new double[] {3.22, 4.2, 1.15, 77.15, 65.2};
+// double[] array = new double[] {3.22, 4.2, 1.15, 77.15, 65.2};  // ручной ввод чисел или рандомный ниже
 
-double Diff(double[] array) 
+double[] arr = new double[5];
+Random random = new Random();
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = random.NextDouble() * 10;
+    Console.WriteLine(Math.Round(arr[i], 2));
+}
+
+void Diff(double[] array)
 {
     double max = array[0];
     double min = array[0];
     double diff = 0;
-    for(int i = 0; i < 5; i++)
+    for (int i = 0; i < 5; i++)
     {
-        if(array[i] > max) max = array[i];
-        if(array[i] <= min) min = array[i];
+        if (array[i] > max) max = array[i];
+        if (array[i] <= min) min = array[i];
         diff = (max - min);
     }
-    
-    return diff;
+    Console.WriteLine($"Разница между максимальным и минимальным элементами массива равна: " + Math.Round(diff, 2));
 }
 
-Console.WriteLine(String.Join(", ", array));
-double dif = Diff(array);
-Console.WriteLine("Разница между максимальныи и минимальным элементов массива равна: " + dif);
+Diff(arr);
+
 
